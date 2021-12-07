8429007285a49079e947ade3e7830ca590555618
zhq0607 <941251339@qq.com>
2021-12-07 13:29:42 +0800

    '完成'

---
+++ js/redirect.js
@@ -0,0 +1,14 @@
+window.addEventListener('load',function(){
+   let header =document.getElementsByTagName('header')[0];
+   header.children[0].ontouchstart=function(){
+       window.location.href='./index.html';
+   }
+ 
+
+
+
+
+
+
+
+})
